0d11c1c172f0c9204c2b8caa388ff96b3e14f07c
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2026-09-23 16:22:30 -0700

    feat: created judge to evaluate responses for tests (question, expects pairs)

---
+++ scorer.py
@@ -0,0 +1,25 @@
+def judge(question, expects, answer, results) -> bool:
+  """
+  q: 'give', expect: 'give'
+  returns whether the expect is in the answer
+  """
+
+  # lower - converts string to lowercase
+  # trim - strips whitespace from beginning and end
+  return expects.lower().trim() in answer.lower()
+  # fails if answer: '0 libraries', expects: 'no libraries', 
+
+  # return any(expects.strip().lower() for chunk in results);
+
+  """
+  Industry practice - LLM as judge
+
+  NLP - as judge
+  Recommended: rapidfuzz - Python library for similarity scoring via natural language processing (no LLM used)
+     - ex. knows that 0 libraries and no libraries semantically mean the same thing
+     - Limitations: only works on small chunks - no large documents -> no vector database, distances, embeddings
+
+  - evaluates by similarity of semantic meaning (regardless of wording), rather than directly checking
+  for exact answer is in the answer
+  - invoke LLM - with prompt, "judge if the expects semantically matches with the answer"
+  """
